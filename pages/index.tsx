ddedc451c71214096f9f9b618430cb5dc78f776d
Kuttyhub <kuttyhub>
2021-12-25 23:27:01 +0530

    winnig logic added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,94 @@
 import type { NextPage } from "next";
 import { useState } from "react";
+import { setTimeout } from "timers";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  const [data, setdata] = useState(
+  const [data, setdata] = useState<(null | string)[][]>(
     Array.from({ length: 3 }, () => Array.from({ length: 3 }, () => null))
   );
   const [isMychance, setisMychance] = useState(true);
   const [isGameStarted, setisGameStarted] = useState(false);
   const [moveCount, setmoveCount] = useState(9);
+  const [alertVisible, setalertVisible] = useState(false);
+  const [alterText, setalterText] = useState("");
 
   const handleClick = (i: any, j: any) => {
     setmoveCount(moveCount - 1);
-    // console.log(i, j);
 
-    if (moveCount === -1) {
-      setisGameStarted(false);
-      setmoveCount(9);
-      alert("game ended");
-      setdata(
-        Array.from({ length: 3 }, () => Array.from({ length: 3 }, () => null))
-      );
-      setisMychance(true);
-      return;
-    }
-    if (!data[i][j] && isGameStarted) {
-      let temp = [...data];
-      temp[i][j] = isMychance ? "O" : "X";
-      setdata(temp);
-      setisMychance(!isMychance);
+    if (isGameStarted) {
+      if (!data[i][j]) {
+        var player = isMychance ? "O" : "X";
+        let temp = [...data];
+        temp[i][j] = player;
+        setdata(temp);
+        var res = checkWinner(i, j, player);
+        console.log(res);
+        if (res == 1) {
+          // alert(player + " Player Wins!");
+          showAlterVisible(player + " Player Wins!");
+          setTimeout(() => resetGame(), 3000);
+        } else if (res == 0) {
+          // alert("Game Tied");
+          showAlterVisible("Game Tied");
+          resetGame();
+        }
+        setisMychance(!isMychance);
+      }
     } else {
       !isGameStarted ? alert("please start the game") : alert("already filled");
     }
+    if (moveCount === 0) {
+      // alert("game ended");
+      showAlterVisible("Game Ended");
+      setTimeout(() => resetGame(), 3000);
+    }
     // console.log(data);
   };
+  const showAlterVisible = (text: string) => {
+    setalertVisible(true);
+    setalterText(text);
+    setTimeout(() => setalertVisible(false), 3000);
+  };
+  const resetGame = () => {
+    setisGameStarted(false);
+    setmoveCount(9);
+    setdata(
+      Array.from({ length: 3 }, () => Array.from({ length: 3 }, () => null))
+    );
+    setisMychance(true);
+  };
 
-  // const checkWinner=(i:any,j:any)=>{
-  //   if ((data[i][j] === data[i+1][j] && data[i][j]===data[i+2]))
+  const checkWinner = (iIdx: any, jIdx: any, player: string) => {
+    //horizontal
+    if (
+      data[0][jIdx] === player &&
+      data[1][jIdx] === player &&
+      data[2][jIdx] === player
+    ) {
+      return 1;
+    }
+    //vertical
+    if (
+      data[iIdx][0] == player &&
+      data[iIdx][1] == player &&
+      data[iIdx][2] == player
+    ) {
+      return 1;
+    }
+    //idagonal
+    if (iIdx == jIdx) {
+      if (
+        data[0][0] == player &&
+        data[1][1] == player &&
+        data[2][2] == player
+      ) {
+        return 1;
+      }
+    }
 
-  // }
+    return moveCount === 0 ? 0 : -1;
+  };
 
   const startGame = () => {
     setisGameStarted(true);
@@ -71,6 +121,7 @@ const Home: NextPage = () => {
           </div>
         ))}
       </div>
+      {alertVisible ? <div className="alert">{alterText}</div> : null}
     </div>
   );
 };
